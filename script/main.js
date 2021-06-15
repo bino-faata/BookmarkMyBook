fe1c568702aeafa108203adcbe054b314524ba90
Forgotten You <maximleluk@gmail.com>
2021-06-15 19:13:32 +0300

    moved to milligram.io css framework. Refactored book Adding and Showing functionality

--- script/main.js
+++ script/main.js
@@ -24,6 +24,11 @@ function app() {
   function addBook(event) {
     event.preventDefault();
 
+    // ** 
+    // * Check if Input field values filled properly
+    // * and subcheck if poster image was added.
+    // * If not - load placeholder image
+    // **
     if (bookInput[0].value.length == 0 ||
         bookInput[1].value.length == 0 ||
         bookInput[2].value.length == 0) {
@@ -32,30 +37,27 @@ function app() {
       if (bookInput[3].value.length == 0) {
         bookInput[3].value = "default_book.png";
       }
+
+      const container = document.querySelector("#booksList");
       const bookDiv = document.createElement('div');
-      bookDiv.classList.add('book');
+      bookDiv.classList.add('column', 'book');
 
       // **
       // * Book item template
       // **
       const bookData = `
-          <img src="${bookInput[3].value}" class="book_poster" alt="Poster" style="width:100%">
-          <div class="container">
-            <p>Author: <b>${bookInput[0].value}</b></p>
-            <p>Title: <b>${bookInput[1].value}</b></p>
-            <p>Genre: <b>${bookInput[2].value}</b></p>
-          </div>`;
+          <img src="${bookInput[3].value}" alt="${bookInput[1].value} Poster">
+            <h5>Author: <b>${bookInput[0].value}</b></h5>
+            <h5>Title: <b>${bookInput[1].value}</b></h5>
+            <h5>Genre: <b>${bookInput[2].value}</b></h5>`;
 
-      const newBook = document.createElement('li');
+      bookDiv.innerHTML = bookData;
 
-      newBook.innerHTML = bookData;
-
-      bookDiv.appendChild(newBook);
-      bookList.appendChild(bookDiv);
+      container.appendChild(bookDiv);
       
       saveToLocalStorage({Author: `${bookInput[0].value}`, Title: `${bookInput[1].value}`, Genre: `${bookInput[2].value}`, img: `${bookInput[3].value}`});
 
-      // ** Clear input fields
+      // ** Clear input fields after adding book
       bookInput[0].value = "";
       bookInput[1].value = "";
       bookInput[2].value = "";
@@ -90,26 +92,21 @@ function app() {
     };
 
     books.forEach((book) => {
-      const bookDiv = document.createElement('div');
-      bookDiv.classList.add('book');
-
-      const newBook = document.createElement('li');
+      const container = document.querySelector("#booksList");
+      const bookDiv = document.createElement("div");
+      bookDiv.classList.add("column", "book");
 
       // **
       // * Template from localStorage:
       // * book.Author, book.Title, book.Genre, book.img
       // **
-      newBook.innerHTML = `
-      <img src="${book.img}" class="book_poster" alt="Poster" style="width:100%">
-        <div class="container">
-          <p>Author: <b>${book.Author}</b></p>
-          <p>Title: <b>${book.Title}</b></p>
-          <p>Genre: <b>${book.Genre}</b></p>
-        </div>`;
+      bookDiv.innerHTML = `
+      <img src="${book.img}" alt="${bookInput[1].value} Poster"> 
+          <h5>Author: <b>${book.Author}</b></h5>
+          <h5>Title: <b>${book.Title}</b></h5>
+          <h5>Genre: <b>${book.Genre}</b></h5>`;
 
-      bookDiv.appendChild(newBook);
-      bookList.appendChild(bookDiv);
-
+      container.appendChild(bookDiv);
     });
   };
 };
